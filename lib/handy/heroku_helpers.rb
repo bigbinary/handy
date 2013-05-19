dc3b67d3bd9507fe1212413564214d3ff75d247f
Neeraj Singh <neerajdotname@gmail.com>
2013-05-19 14:00:33 -0400

    pass production

--- lib/handy/heroku_helpers.rb
+++ lib/handy/heroku_helpers.rb
@@ -20,7 +20,7 @@ namespace :handy do
 
     desc "Takes snapshot of production db and copies production data to staging"
     task :prod2staging, :app do |t, args|
-      take_current_snapshot heroku_app_name(t, args)
+      take_current_snapshot "#{heroku_app_name(t, args)}-production"
 
       heroku_app_name = heroku_app_name(t, args)
       src_app_name = "#{heroku_app_name}-production"
