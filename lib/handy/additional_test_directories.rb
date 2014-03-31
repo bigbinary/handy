766b2b23855e39a92782b6480a2ef2b47093b34c
Dmitriy Dudkin <dudkin.dmitriy@gmail.com>
2014-03-31 23:52:12 +0300

    Additional test tasks no longer running separately from default

--- lib/handy/additional_test_directories.rb
+++ lib/handy/additional_test_directories.rb
@@ -1,32 +1,25 @@
 # Adding test/services directory to the rake test.
 namespace :test do
   desc "Test services code"
-  Rake::TestTask.new(:services) do |t|
-    t.libs << "test"
+  Rails::TestTask.new(services: 'test:prepare') do |t|
     t.pattern = 'test/services/**/*_test.rb'
-    t.verbose = true
   end
 end
-Rake::Task[:test].enhance { Rake::Task["test:services"].invoke }
 
 # Adding test/workers directory to the rake test.
 namespace :test do
   desc "Test workers code"
-  Rake::TestTask.new(:workers) do |t|
-    t.libs << "test"
+  Rails::TestTask.new(workers: 'test:prepare') do |t|
     t.pattern = 'test/workers/**/*_test.rb'
-    t.verbose = true
   end
 end
-Rake::Task[:test].enhance { Rake::Task["test:workers"].invoke }
 
 # Adding test/carriers directory to the rake test.
 namespace :test do
   desc "Test carriers code"
-  Rake::TestTask.new(:carriers) do |t|
-    t.libs << "test"
+  Rails::TestTask.new(carriers: 'test:prepare') do |t|
     t.pattern = 'test/carriers/**/*_test.rb'
-    t.verbose = true
   end
 end
-Rake::Task[:test].enhance { Rake::Task["test:carriers"].invoke }
+
+Rake::Task['test:run'].enhance ["test:services", "test:carriers", "test:workers"]
