cf9de1c7d60706c823e0bb95db2256e333fb1ee2
Neeraj Singh <neeraj@bigbinary.com>
2015-06-24 16:22:15 -0400

    test directories should have been deleted

--- lib/handy/engine.rb
+++ lib/handy/engine.rb
@@ -5,7 +5,6 @@ module Handy
       load 'handy/trailing_whitespaces.rb'
       load 'handy/delete_merged_branches.rb'
       load 'handy/heroku_tasks.rb'
-      load 'handy/additional_test_directories.rb'
       load 'handy/delete_heroku_apps.rb'
       load 'handy/add_database_yml.rb'
     end
