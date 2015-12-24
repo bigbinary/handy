53859f7d74dd67a765a13f185e0efadf9c74a002
Prathamesh Sonpatki <csonpatki@gmail.com>
2015-12-24 17:31:39 +0530

    Load Settings from settings.yml if this file is present in the project
    - This gives old projects which are using `Settings` ability to use
      latest version of handy.
    - In new projects, we have already removed `settings.yml` from `config`
      folder.
    - So this can be workable compromise for old as well as new apps.

--- lib/handy/engine.rb
+++ lib/handy/engine.rb
@@ -1,6 +1,13 @@
 module Handy
   class Engine < ::Rails::Engine
 
+    initializer :load_application_yml, before: :load_environment_config do
+      settings_file = Rails.root.join('config', 'settings.yml')
+      if File.exist?(settings_file)
+        ::Settings = ConfigLoader.new('settings.yml').load
+      end
+    end
+
     rake_tasks do
       load 'handy/trailing_whitespaces.rb'
       load 'handy/delete_merged_branches.rb'
